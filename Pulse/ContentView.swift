837d51e0020c8e9050002d951c25fd422752907a
winebarrel <sugawara@winebarrel.jp>
2024-11-17 15:07:13 +0900

    first commit

---
+++ Pulse/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pulse
+//
+//  Created by 菅原元気 on 2024/11/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pulse/PulseApp.swift
@@ -0,0 +1,17 @@
+//
+//  PulseApp.swift
+//  Pulse
+//
+//  Created by 菅原元気 on 2024/11/17.
+//
+
+import SwiftUI
+
+@main
+struct PulseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
