0e5872f75d19d6caaac67424adc1adc64220c688
winebarrel <sugawara@winebarrel.jp>
2024-11-24 23:33:17 +0900

    Add github link

--- Pulse/RightClickMenuView.swift
+++ Pulse/RightClickMenuView.swift
@@ -1,10 +1,21 @@
 import SwiftUI
 
 struct RightClickMenuView: View {
+    @AppStorage("githubQuery") private var githubQuery = Constants.defaultGithubQuery
+
     var body: some View {
         Divider()
-        Button("TODO") {
-            // TODO:
+        Button("Pull Requests") {
+            Task {
+                let url = URL(string: "https://github.com/pulls?q=" + (githubQuery.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? ""))!
+                NSWorkspace.shared.open(url)
+            }
+        }
+        Button("Issues") {
+            Task {
+                let url = URL(string: "https://github.com/issues")!
+                NSWorkspace.shared.open(url)
+            }
         }
         Divider()
         SettingsLink {
