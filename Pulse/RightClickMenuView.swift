555917a7f31b0c67fd605613897bed6bcbc212a4
winebarrel <sugawara@winebarrel.jp>
2024-11-24 12:39:45 +0900

    Add SettingsView

--- Pulse/RightClickMenuView.swift
+++ Pulse/RightClickMenuView.swift
@@ -2,6 +2,17 @@ import SwiftUI
 
 struct RightClickMenuView: View {
     var body: some View {
+        Divider()
+        Button("TODO") {
+            // TODO:
+        }
+        Divider()
+        SettingsLink {
+            Text("Settings")
+        }.preActionButtonStyle {
+            NSApp.activate(ignoringOtherApps: true)
+        }
+        Divider()
         Button("Quit") {
             NSApplication.shared.terminate(self)
         }
