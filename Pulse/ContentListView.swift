3fc40597a607d184d7bf388975b78b936c220ac0
winebarrel <sugawara@winebarrel.jp>
2024-11-30 17:26:09 +0900

    Add ContentListView

---
+++ Pulse/ContentListView.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+struct ContentListView: View {
+    let pulls: PullRequests
+
+    var body: some View {
+        // TODO:
+        Text("ContentListView")
+    }
+}
--- Pulse/PullRequestListView.swift
+++
@@ -1,8 +0,0 @@
-import SwiftUI
-
-struct PullRequestListView: View {
-    var body: some View {
-        // TODO:
-        Text("PullRequestListView")
-    }
-}
