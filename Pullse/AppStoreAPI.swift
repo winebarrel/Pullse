50f132bcb6e59f657f20b01d32cda1a5352b621e
winebarrel <sugawara@winebarrel.jp>
2025-04-07 01:57:00 +0900

    Check update

---
+++ Pullse/AppStoreAPI.swift
@@ -0,0 +1,35 @@
+import Foundation
+
+struct AppStoreRespResult: Codable {
+    var version: String
+}
+
+struct AppStoreResp: Codable {
+    var resultCount: Int
+    var results: [AppStoreRespResult]
+}
+
+enum AppStoreAPI {
+    static func getInfo(_ bundleId: String) async -> AppStoreRespResult? {
+        do {
+            var req = URLRequest(url: URL(string: "https://itunes.apple.com/br/lookup?bundleId=\(bundleId)")!)
+            let (data, rawResp) = try await URLSession.shared.data(for: req)
+
+            guard let resp = rawResp as? HTTPURLResponse else {
+                fatalError("failed to cast URLResponse to HTTPURLResponse")
+            }
+
+            if resp.statusCode != 200 {
+                return nil
+            }
+
+            let decoder = JSONDecoder()
+            let info = try decoder.decode(AppStoreResp.self, from: data)
+
+            return info.results.first
+        } catch {
+            Logger.shared.error("AppStore request error: \(error)")
+            return nil
+        }
+    }
+}
